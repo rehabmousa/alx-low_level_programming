0eb0e100b39da6fd6bb4fe236dbb3e7010c93e7b
rehabmousa <rehabelsayed325@gmail.com>
2023-05-02 18:01:23 -0700

    go

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,7 +4,7 @@
  * swap_int - swaps the value of two integers
  *		using two input parameters
  *
- * @n: input parameter 1
+ * @a: input parameter 1
  * @b: input parameter 2
  *
  * Return: Nothing
